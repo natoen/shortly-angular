0cf2a63846d69886f794723e448c712ca5131e86
natoen <hgmatsuda@gmail.com>
2016-04-03 08:23:49 +0900

    (feat) Add validation

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,9 +1,14 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
   $scope.link = {};
-  $scope.addLink = function () {
-    Links.addOne($scope.link);
+  $scope.addLink = function (valid) {
+    console.log($scope.link);
+    if (valid) {
+      Links.addOne($scope.link, function () {
+        $scope.link.url = '';
+      });
+    }
+    // $scope.link.url = '';
   };
 });
