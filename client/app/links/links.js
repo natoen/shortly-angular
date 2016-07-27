818ffea707d5fa1fead126e44980438fef707444
natoen <hgmatsuda@gmail.com>
2016-07-28 07:33:35 +0900

    (feat) Add styling

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,7 +2,6 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links, Auth) {
   $scope.data = {};
-  
   $scope.signout = Auth.signout;
 
   Links.getAll().then(function (res) {
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,13 +2,15 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links, Auth) {
   $scope.link = {};
-
+  $scope.loading = true;
   $scope.signout = Auth.signout;
 
   $scope.addLink = function (valid) {
     if (valid) {
+      $scope.loading = false;
       Links.addOne($scope.link, function () {
         $scope.link.url = '';
+        $scope.loading = true;
       });
     }
   };
